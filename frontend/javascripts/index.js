1d06367628e12b83c3314ab5a7e52725e43833f6
Dimitry Sparrow <info@sparrow.96.lt>
2016-10-27 20:03:21 +0300

    3

--- frontend/javascripts/index.js
+++ frontend/javascripts/index.js
@@ -5,81 +5,22 @@ requirejs.config({
 requirejs([
 	'domReady!',
 	'jquery',
-	'slick',
-	'mediaelement-and-player'
-
+	'jquery-ui',
+	'slick'
 
 ], function () {
 	'use strict';
 
-	$('.main-slider').slick({
-		dots: true,
+	$( ".accordion" ).accordion();
+	$( ".accordion" ).accordion({
+		collapsible: true,
+		active: false
+	});
+
+	$('.slider').slick({
 		autoplay: true,
 		autoplaySpeed: 2500
 	});
-
-	$('.vertical-slider').slick({
-		vertical: true,
-		slidesToShow: 5
-	});
-
-	$(".block-slider").slick({
-
-		slidesToShow: 5,
-		slidesToScroll: 1,
-
-
-		responsive: [{
-
-			breakpoint: 1500,
-			settings: {
-				slidesToShow: 4,
-			}
-
-			}, {
-
-			breakpoint: 1300,
-			settings: {
-				slidesToShow: 3,
-			}
-
-			}, {
-
-			breakpoint: 965,
-			settings: {
-				slidesToShow: 2,
-			}
-
-			}, {
-				breakpoint: 700,
-				settings: {
-					slidesToShow: 1,
-			}
-		}]
-	});
-
-	var menu = {
-		init: function(){
-			this.btnMenu();
-		},
-		btnMenu: function() {
-			$('.btn-menu').on('click', function(){
-				if(!$(this).hasClass('active-btn')){
-					$(this).addClass('active-btn');
-					$(this).next().slideDown('fast');
-				} else {
-					$(this).removeClass('active-btn');
-					$(this).next().slideUp('fast');
-				}
-			});
-		}
-	};
-
-	menu.init();
-
-	var player = new MediaElementPlayer('.player1');
-
-
 });
 
 
